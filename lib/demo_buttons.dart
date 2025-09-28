ba20c90ee6817be3c364cdd9e645d9e2ad6ef3ec
Riddhi138 <147044631+Riddhi138@users.noreply.github.com>
2025-09-28 22:41:14 +0530

    Todo App Code Commits

---
+++ lib/demo_buttons.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class DemoButtons extends StatefulWidget {
+  const DemoButtons({super.key});
+
+  @override
+  State<DemoButtons> createState() {
+    return _DemoButtonsState();
+  }
+}
+
+class _DemoButtonsState extends State<DemoButtons> {
+  var _isUnderstood = false;
+//override build method
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisSize: MainAxisSize.min,     //to set the the size as uch needed for the content
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _isUnderstood = false;
+                });
+              },
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _isUnderstood = true;
+                });
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        ),
+        if (_isUnderstood) const Text('Awesome!'),
+      ],
+    );
+  }
+}
